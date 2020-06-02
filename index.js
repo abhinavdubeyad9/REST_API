ec4ee2cb61306a28caa377b11d24b5e24777139a
abhinavdubeyad9 <dubey.abhinav76@gmail.com>
2020-06-02 22:44:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+    
+const bodyParser = require('body-parser');
+// set up express app
+const app = express();
+
+app.use(bodyParser.json());
+
+//intialize routes
+app.use('/api',require('./routes/api'));
+
+//listen for requests
+app.listen(process.env.port || 4000, function(){
+     console.log('now listening for requests'); 
+});
+
+
